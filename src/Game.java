18681ae8be926ee9689408b4689900339eaccbd1
JPWetstein <Jeep.Wetstein@gmail.com>
2014-01-31 17:25:34 -0500

    Added red dice

--- src/Game.java
+++ src/Game.java
@@ -18,19 +18,21 @@ public class Game
 		r = new Random(System.currentTimeMillis());
 		diceCup = new Stack<Dice>();
 		
+		//Green dice
 		for(int i = 0; i < 6; i++)
 		{
 			diceCup.push(new Dice(2,3,1,"Green", r));
 		}
-		
+		//Yellow Dice - default
 		for(int i = 0; i < 4; i++)
 		{
 			diceCup.push(new Dice(r));
 		}
 		
+		//Red dice
 		for(int i =0; i < 3; i++)
 		{
-			diceCup.push(new Dice(r));
+			diceCup.push(new Dice(2, 1, 3, "Red", r));
 		}
 	}
 	
